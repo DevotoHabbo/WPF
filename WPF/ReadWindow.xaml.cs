675c7d1a6d6fa947a69db02e7f646b9ada4378cb
Phuong Nguyen Duy <ferbious21@gmail.com>
2020-11-11 16:30:17 -0500

    Add project files.

---
+++ WPF/ReadWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF
+{
+    /// <summary>
+    /// Interaction logic for ReadWindow.xaml
+    /// </summary>
+    public partial class ReadWindow : Window
+    {
+        public ReadWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Display_Click(object sender, RoutedEventArgs e)
+        {
+            NABAEntities4 db = new NABAEntities4();
+            DataDisplay.ItemsSource = db.People.ToList();
+        }
+
+        private void CloseDisplay_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ WPF/WriteWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF
+{
+    /// <summary>
+    /// Interaction logic for WriteWindow.xaml
+    /// </summary>
+    public partial class WriteWindow : Window
+    {
+
+        public WriteWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            NABAEntities4 db = new NABAEntities4();
+            Person obj = new Person();
+            obj.Id = Convert.ToInt32(db.People.Count());
+            obj.Firstname = FirstName.Text;
+            obj.Lastname = LastName.Text;
+
+            db.People.Add(obj);
+            db.SaveChanges();
+            this.Close();
+
+        }
+    }
+}
